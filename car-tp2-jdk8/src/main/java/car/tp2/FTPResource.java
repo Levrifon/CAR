062570a980f0b5c3c50ee3d9b11a25f9eb079295
remy <rems-msn@hotmail.fr>
2016-03-13 15:07:21 +0100

    try to do POST

--- car-tp2-jdk8/src/main/java/car/tp2/FTPResource.java
+++ car-tp2-jdk8/src/main/java/car/tp2/FTPResource.java
@@ -1,11 +1,11 @@
 package car.tp2;
 
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.SocketException;
 
 import javax.ws.rs.GET;
@@ -20,6 +20,8 @@ import org.apache.commons.net.ftp.FTPClient;
 @Path("/ftp")
 public class FTPResource {
 	FTPClient client;
+	final String url = "localhost:8080/rest/tp2/ftp"; 
+	final String accessForbidden = "<h1> You are not connected, no access is possible </h1>";
 	@GET
 	@Produces("text/html")
 	public String sayHello() {
@@ -32,7 +34,6 @@ public class FTPResource {
 	public StreamingOutput get(@PathParam("path") String path) {
 		
 		StreamingOutput outputstream = null;
-		StreamingOutput streamingOutput;
 		try {
 			final InputStream inputstream = client.retrieveFileStream(path);
 			outputstream = new StreamingOutput() {
@@ -43,13 +44,11 @@ public class FTPResource {
 					while((lect = inputstream.read()) != -1) {
 						os.write(lect);
 					}
-					
 				}
 			};
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 		return outputstream;
 		
 	}
@@ -66,7 +65,13 @@ public class FTPResource {
 	@Produces("text/html")
 	@Path("/connect/{user}/{pwd}")
 	public String connect(@PathParam("user") String user ,@PathParam("pwd") String pwd){
-		boolean connection;
+		boolean connection = false;
+		/* si l'utilisateur est déjà connecte on affiche un message le précisant */
+		if(client != null) {
+			if(client.isConnected()) {
+				return "<h2><p> Vous etes deja connecte + " + user + " </p></h2>";
+			}
+		}
 		try {
 			client = new FTPClient();
 			client.connect("localhost",4444);
@@ -76,8 +81,11 @@ public class FTPResource {
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-		System.out.println("Termine");
-		return "<h2><p>Bienvenue "  + user + " ! </p></h2>";
+		/* si la connexion a reussi (bon login et mot de passe */
+		if(connection){
+			return "<h2><p>Bienvenue "  + user + " ! </p></h2>";
+		}
+		return "<h2><p> Mauvais identifiant et/ou mot de passe </p></h2>";
 	}
 	@GET
 	@Produces("text/html")
@@ -95,13 +103,66 @@ public class FTPResource {
 		}else {
 			builderhtml.append("<table>");
 			for (String file : filelist) {
-				builderhtml.append("<tr><td><p>");
+				builderhtml.append("<tr><td><p><a href="+this.url+"/file/"+file+">");
 				builderhtml.append(file);
-				builderhtml.append("</p></td></tr>");
+				builderhtml.append("</a></p></td></tr>");
 			}
 			builderhtml.append("</table>");
 		}
 		System.out.println(client.getReplyString());
 		return builderhtml.toString();
+	}
+	
+	@GET
+	@Produces("text/html")
+	@Path("/pwd/")
+	public String pwd() {
+		try {
+			return "<h1>" +client.printWorkingDirectory() + "</h1>";
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "<h1> Problem during access of Working Directory </h1>";
+	}
+	@GET
+	@Produces("text/html")
+	@Path("/cwd/{directory}")
+	public String cwd(@PathParam("directory") String directory) {
+		if(!client.isConnected()) {
+			return accessForbidden;
+		}
+		try {
+			client.cwd(directory);
+		} catch (IOException e) {
+			return "<h1> Erreur lors du changement de directory </h1>";
+		}
+		try {
+			return "<h1> Current directory is : " + client.printWorkingDirectory() + "</h1>";
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "<h1> Erreur lors du changement de directory </h1>";
+	}
+
+	@POST
+	@Produces("text/html")
+	@Path("/post/{file}")
+	public String postFile(@PathParam("file") String file) {
+		InputStream input;
+		if (!client.isConnected()) {
+			return accessForbidden;
+		}
+		boolean termine = false;
+		try {
+			input = new FileInputStream(file);
+			termine = client.storeFile(file, input);
+			input.close();
+		} catch (IOException e) {
+			return "<h1>" + client.getReplyString() + "</h1>";
+		}
+		if (termine) {
+			return "<h1> Transfert terminé </h1>";
+		}
+		return "<h1> Transfert interrompu </h1>";
 	}
 }
